9550233ee98c8ca5ad99cbbe3e0d69d9b874b82e
Prerna_27 <105448682+mukesh009sharma@users.noreply.github.com>
2024-10-01 23:55:34 +0530

    Create Abstraction.java

---
+++ Abstraction.java
@@ -0,0 +1,17 @@
+abstract class Animal {
+    abstract void makeSound();
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
